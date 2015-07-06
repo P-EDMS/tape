a80b786b2ab28495495fb458ff818bac28765b6b
roylee0704 <roylee0704@gmail.com>
2015-07-06 23:28:44 +0800

    initial commit

---
+++ src/main/java/com/panton/tape/WordIndexer.java
@@ -0,0 +1,7 @@
+package com.panton.tape;
+
+/**
+ * Created by roylee on 7/6/15.
+ */
+public class WordIndexer {
+}
---
+++ src/test/java/com/panton/tape/WordIndexerTest.java
@@ -0,0 +1,19 @@
+package com.panton.tape;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+/**
+ * Created by roylee on 7/6/15.
+ */
+public class WordIndexerTest {
+
+
+    @Test public void dummyTest() throws Exception {
+
+        assertThat(true).isTrue();
+    }
+
+}
